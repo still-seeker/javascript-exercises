8b8fb9b589744b649227d6ca19d2d0379434a437
Still-Seeker <151577316+still-seeker@users.noreply.github.com>
2025-01-01 18:45:20 +0200

    finish exercise 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,12 +1,25 @@
 const repeatString = function(str, num) {
+    if (num < 0) {
+            return "ERROR";
+            
+        }
     let repeatedString = "";
     while (num > 0){
-        repeatedString += str;
-        num --;
+                
+        if (num === 0){
+            repeatedString = '';
+        }
+        else if (num > 0){
+            repeatedString += str;
+            num --;
+        }      
+        
+        
     }
     return repeatedString;    
 };
-repeatString('hey', 3);
+
+
 
 // Do not edit below this line
 module.exports = repeatString;
