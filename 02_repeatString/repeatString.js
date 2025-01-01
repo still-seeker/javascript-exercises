376b079c9138e3657e39d388b149e4de1e48753d
Still-Seeker <151577316+still-seeker@users.noreply.github.com>
2025-01-01 16:15:39 +0200

    complete repeatString function

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    let repeatedString = "";
+    while (num > 0){
+        repeatedString += str;
+        num --;
+    }
+    return repeatedString;    
 };
+repeatString('hey', 3);
 
 // Do not edit below this line
 module.exports = repeatString;
