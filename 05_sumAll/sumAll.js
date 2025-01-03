e2de4c781ccfa58e9b15944634ba29127e34204e
Still-Seeker <151577316+still-seeker@users.noreply.github.com>
2025-01-03 04:59:35 +0200

    solve sumAll function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(min, max) { 
+    if(!Number.isInteger(min) || !Number.isInteger(max)){
+        return "ERROR";
+    }
+    if (min < 0 || max < 0) return "ERROR";
+    if (min > max){
+        [min, max] = [max, min];
+    }
+    let finalSum = 0;
+    let i = min;
+    while (i <= max){
+        finalSum += i;
+        i++;
+    }
+    return finalSum;
 };
 
 // Do not edit below this line
