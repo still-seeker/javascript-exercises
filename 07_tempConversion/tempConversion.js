6bd25cafabe1efed6a58632d7ffa3773906681fa
Still-Seeker <151577316+still-seeker@users.noreply.github.com>
2025-01-04 05:07:31 +0200

    Do temperature conversion exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degrees) {
+  let calc = (degrees - 32) * (5/ 9);
+  return Math.round(calc * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degrees) {
+  const  calc1 = (degrees * (9 /5) + 32);
+  return Math.round(calc1 * 10) / 10;
 };
 
 // Do not edit below this line
