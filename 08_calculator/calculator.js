5ece2f21e7d394274ef06f9a78993b92e5086e4b
Still-Seeker <151577316+still-seeker@users.noreply.github.com>
2025-02-17 06:18:09 +0200

    Finish 08_calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	  const sum =  num1 + num2;
+    return sum;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+  const minus = num1 - num2;
+  return minus;	
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  	return array.reduce((total, curr) => total + curr, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((acc, curr) => acc * curr, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let fact;
+	if (num > 1) {
+    fact = num * factorial(num - 1);
+  }
+  else 
+    fact = 1;
+
+  return fact;
 };
 
 // Do not edit below this line
