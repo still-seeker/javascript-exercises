d4e6315915d1aeee7353f9ad25295261b916a8a1
Still-Seeker <151577316+still-seeker@users.noreply.github.com>
2025-02-17 07:26:08 +0200

    A function to check for palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,20 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    //All the characters expected in a string
+    const alphaNum = "abcdefghijhklmnopqrstuvwxyz0123456789";
 
+    let palStr = string
+                    .toLowerCase()
+                    .split("")
+                    .filter((char) => alphaNum.includes(char))
+                    .join("");
+    
+    const reverseString = palStr.split('').reverse().join('');
+    
+    //check equality of both reverseString and palStr
+    if (reverseString === palStr)
+        return true;
+    return false;
+   
 };
 
 // Do not edit below this line
