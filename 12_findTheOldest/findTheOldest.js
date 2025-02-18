5f8f0f1e7c5c8cd5e8207c60d89102eca450f7ae
Still-Seeker <151577316+still-seeker@users.noreply.github.com>
2025-02-18 17:57:03 +0200

    Finish JAVASCRIPT_EXERCISES

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return people.reduce((oldest, current) => {
+        //Handling undefined yearOfDeath
+        if (current.yearOfDeath === undefined) {
+            let currentYear = new Date().getFullYear();
+            current.yearOfDeath = currentYear;
+        }
+        else {
+            current.yearOfDeath = current.yearOfDeath;
+        }
 
+        // computes and compares the age between objects
+        return (current.yearOfDeath - current.yearOfBirth) > (oldest.yearOfDeath - oldest.yearOfBirth) ?
+            current : oldest; 
+    }, people[0])       
 };
 
 // Do not edit below this line
