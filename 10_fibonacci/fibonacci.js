8dff9274f37abf12801f633dc026be3197e411c9
Still-Seeker <151577316+still-seeker@users.noreply.github.com>
2025-02-18 03:41:21 +0200

    Add function to get location of fibonacci in the sequence

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    const fibo = [0, 1];
 
+    let count;
+    if (typeof num !== 'number') {
+        count = parseInt(num);
+    }
+    else {
+        count = num;
+    }
+
+    if (count < 0) return "OOPS";
+    if (count === 0) return 0;
+
+    for (let i = 2; i <= count; i++){
+        fibo[i] = fibo[i-1] + fibo[i - 2];
+    }
+    return fibo[count];
 };
 
 // Do not edit below this line
